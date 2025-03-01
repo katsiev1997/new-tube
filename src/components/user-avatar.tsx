82903f739ce99fc4d88a18fae72a410cee58ca56
katsiev1997 <katsiev1997@gmail.com>
2025-03-01 14:14:18 +0300

    Studio layout

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,33 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { cn } from "@/lib/utils";
+
+const avatarVariants = cva("", {
+	variants: {
+		size: {
+			default: "w-9 h-9",
+			xs: "w-4 h-4",
+			sm: "w-6 h-6",
+			lg: "w-10 h-10",
+			xl: "h-[160px] w-[160px]",
+		},
+	},
+	defaultVariants: {
+		size: "default",
+	},
+});
+
+interface UserAvatarProps extends VariantProps<typeof avatarVariants> {
+	imageUrl: string;
+	name: string;
+	className?: string;
+	onClick?: () => void;
+}
+
+export const UserAvatar = ({ imageUrl, name, className, onClick, size }: UserAvatarProps) => {
+	return (
+		<Avatar className={cn(avatarVariants({ size, className }))} onClick={onClick}>
+			<AvatarImage src={imageUrl} alt={name} />
+		</Avatar>
+	);
+};
--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
-import { UserCircleIcon } from "lucide-react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react";
 
 export const AuthButton = () => {
 	// TODO: add diferent auth states
 	return (
 		<>
 			<SignedIn>
-				<UserButton />
+				<UserButton>
+					<UserButton.MenuItems>
+						{/* TODO: Add user profile menu  */}
+						<UserButton.Link
+							label="Studio"
+							href="/studio"
+							labelIcon={<ClapperboardIcon className="size-4" />}
+						/>
+						<UserButton.Action label="manageAccount" />
+					</UserButton.MenuItems>
+				</UserButton>
 				{/* TODO: add menu items for Studio and User profile */}
 			</SignedIn>
 			<SignedOut>
